963ba815c6ce7a3609ccc7d217ee342e73eb7db3
Kaustubh Shukla <your-email@example.com>
2026-06-15 14:31:03 +0530

    To-Do app completed. Final push for project 1

--- todoList/src/main/java/com/todo/todoapp/controller/TaskController.java
+++ todoList/src/main/java/com/todo/todoapp/controller/TaskController.java
@@ -4,16 +4,40 @@ import com.todo.todoapp.models.Task;
 import com.todo.todoapp.services.TaskService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+//@RequestMapping("/tasks")
 public class TaskController {
     private final TaskService taskService;
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
-    public String getTasks() {
+    @GetMapping
+    public String getTasks(Model model) {
         List<Task> tasks = taskService.getAllTasks();
-        return "Tasks";
+        model.addAttribute("tasks", tasks);
+        return "tasks";
     }
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+
 }
--- todoList/src/main/java/com/todo/todoapp/services/TaskService.java
+++ todoList/src/main/java/com/todo/todoapp/services/TaskService.java
@@ -1,6 +1,40 @@
 package com.todo.todoapp.services;
 
+import com.todo.todoapp.models.Task;
+import com.todo.todoapp.repository.TaskRepository;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TaskService {}
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+
+
+    }
+
+    public void toggleTask(Long id) {
+        Task task = taskRepository.findById(id)
+        .orElseThrow(()->new IllegalArgumentException("Invalid task ID"));
+                task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
+}
